e2d725c6172a6f0ad1459c49962a7c337ae5d997
yechielvogel <yechielvogel@gmail.com>
2025-03-17 22:28:04 -0400

    [+] added upside down text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,7 +24,7 @@ class MessageScreen extends StatefulWidget {
 class _MessageScreenState extends State<MessageScreen> {
   final List<Map<String, dynamic>> messages = [
     {
-      'text': 'Hi Mirel, good morning! Tap here to continue.',
+      'text': 'Hi Mirel, good morning! tap here to continue.',
       'color': Colors.blue
     },
     {'text': 'Tap again', 'color': Colors.green},
@@ -33,8 +35,13 @@ class _MessageScreenState extends State<MessageScreen> {
     {'text': 'Drive well 🚙', 'color': Colors.yellow},
     {'text': 'Remember those red light things 🚦', 'color': Colors.pink},
     {'text': "They're not a suggestion", 'color': Colors.indigo},
-    {'text': 'Mirel', 'color': Colors.cyan},
-    {'text': 'Just thought I would wish you', 'color': Colors.lime},
+    {
+      'text': 'Why is this upside down?',
+      'color': Colors.cyan,
+      'rotate': true,
+    },
+    {'text': 'I think you might have pressed too hard', 'color': Colors.purple},
+    {'text': 'Anyway just thought I would wish you', 'color': Colors.lime},
     {'text': 'Allot of hatzlacha', 'color': Colors.amber},
     {'text': 'Have a good one', 'color': Colors.deepOrange},
     {'text': 'Thats it', 'color': Colors.brown},
@@ -56,10 +63,12 @@ class _MessageScreenState extends State<MessageScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final currentMessage = messages[currentIndex];
+
     return Scaffold(
       body: AnimatedContainer(
         duration: Duration(seconds: 1),
-        color: messages[currentIndex]['color'],
+        color: currentMessage['color'],
         child: Center(
           child: GestureDetector(
             onTap: _nextMessage,
@@ -68,16 +77,30 @@ class _MessageScreenState extends State<MessageScreen> {
               transitionBuilder: (child, animation) {
                 return ScaleTransition(scale: animation, child: child);
               },
-              child: Text(
-                messages[currentIndex]['text'],
-                key: ValueKey<int>(currentIndex),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
+              child: currentMessage['rotate'] == true
+                  ? Transform.rotate(
+                      angle: 3.14, // Rotate 180 degrees
+                      child: Text(
+                        currentMessage['text'],
+                        key: ValueKey<int>(currentIndex),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  : Text(
+                      currentMessage['text'],
+                      key: ValueKey<int>(currentIndex),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
             ),
           ),
         ),
